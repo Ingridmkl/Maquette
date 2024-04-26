d6cc928b0106c609c501271f0a063853a903f45f
Ingrid <ingrid-rose.moukala@outlook.fr>
2024-04-26 11:36:55 +0200

    ok

---
+++ Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Designed by AuDesign</p>
+        </footer>
+    );
+}
+
+export default Footer
--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+import App from './App.js'
+
 function clickmenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".corps-icon");
