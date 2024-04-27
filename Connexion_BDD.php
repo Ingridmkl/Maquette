9a37e044f1f18b4d5699c64b937907a75bd9c178
astou0803ndongo <asnd62548@eleve.isep.fr>
2024-04-28 00:11:35 +0200

    cgu + php + modifs creationcompte

---
+++ Connexion_BDD.php
@@ -0,0 +1,21 @@
+<?php
+
+//c'est le code pour faire le lien avec la bdd
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "maquettedatabase";
+
+// Créer une connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//à chaque fois qu'une page web .html aura besoin d'utiliser la base de data, il faudra "l'appeler" avec : 
+//<?php include 'Connexion_BDD.php'; point d'interrogation fermer le chevron
+
+?>
