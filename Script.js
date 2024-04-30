c8fd8198624cde19207736c5f949005e5b2596b3
Ingrid <ingrid-rose.moukala@outlook.fr>
2024-04-30 17:13:01 +0200

    JS

--- Script.js
+++ Script.js
@@ -12,7 +12,7 @@ const search = () =>{
     const searchbox = document.getElementById("search-item").value.toUpperCase();
     const items = document.getElementById("product-list")
     const product = document.querySelectorAll(".product")
-    const pname = document.getElementsByTagName("h2")
+    const pname = items.getElementsByTagName("h2")
 
     for(var i=0; i < pname.length;i++){
         let match = product[i].getElementsByTagName('h2')[0];
