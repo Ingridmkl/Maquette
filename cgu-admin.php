d3ccbca6c3b63767af8653e4ed2d825ff2ba8642
Fitzsolinio <journeleliott@gmail.com>
2024-06-19 13:01:46 +0200

    correction de détails

--- cgu-admin.php
+++ cgu-admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CGU</title>
+    <title>CGU admin</title>
     <link rel="stylesheet" href="cgu.css">
     <link rel="stylesheet" href="media.css">
     <script src="https://kit.fontawesome.com/dcfda6ef51.js"></script>
--- cgu.php
+++ cgu.php
@@ -90,10 +90,10 @@
                 <ul>
                     <li><a href="index.php">Accueil</a></li>
                     <li><a href="index.php">Le produit</a></li>
-                    <li><a href="#search">Nos partenaires</a></li>
+                    <li><a href="Search.php">Nos partenaires</a></li>
                     <li><a href="Page_A_propo.html">A propos de nous</a></li>
                     <li><a href="contact.html">Formulaire</a></li>
-                    <li><a href="FAQ.php">FAQ</a></li>
+                    <li><a href="FAQ.html">FAQ</a></li>
                     <li><a href="forum.php">Forum</a></li>
                 </ul>
             </div>
@@ -104,6 +104,5 @@
         </div>
     </footer>
     <script src="Script.js"></script>
-
 </body>
 </html>
